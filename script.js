fc52f70ab8bc334922873040b13cc427e16176ae
avikasingh807 <avikadangi2006@gmail.com>
2026-06-21 10:47:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeImage(element) {
-  document.getElementById("mainImage").src = element.src;
-}
-
-function addToCart() {
-  document.getElementById("cartMsg").innerText = "✔ Added to Cart!";
-}
